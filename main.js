45716c91b97b8c0ffa8fb2f2db69f0fe9f19d5be
Giampiero Bonifazi <gpbonifazi@gmail.com>
2023-05-28 23:59:39 +0200

    L2

--- main.js
+++ main.js
@@ -1,2 +1,9 @@
 // Giampiero Bonifazi 
-const product = 'Socks'
+const app = Vue.createApp({
+    data() {
+        return {
+            product: 'Socks',
+            description: 'A warm fuzzy pair of socks.' 
+        }
+    }
+})
